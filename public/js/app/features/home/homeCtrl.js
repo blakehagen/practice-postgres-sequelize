e766f9ecc1d2c6cf1827c0bfb32f8921cc3e2fb4
Blake Hagen <blakehagen@gmail.com>
2016-07-23 13:46:25 -0600

    added service

--- public/js/app/features/home/homeCtrl.js
+++ public/js/app/features/home/homeCtrl.js
@@ -1,7 +1,15 @@
 'use strict';
 
-angular.module('pgPractice').controller('homeCtrl', function () {
+angular.module('pgPractice').controller('homeCtrl', function (mainService) {
   var home = this;
+
+  home.addUser = function () {
+    console.log('home.newUser on service!', home.newUser);
+    mainService.addNewUser(home.newUser);
+    home.newUser = null;
+  };
+
+
 
 
 
---
+++ public/js/app/services/mainService.js
@@ -0,0 +1,7 @@
+angular.module('pgPractice').service('mainService', function () {
+
+  this.addNewUser = function (newUser) {
+    console.log('newUser', newUser);
+  };
+
+});
